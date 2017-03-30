684220d69c34027bc6255cceb39d1949ed8f20c8
Felipe <favazquez@uc.cl>
2017-03-30 20:04:00 -0300

    create and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,43 @@
 class UsersController < ApplicationController
   def create
-    render json: {status:"trying to create"}
+    if params[:id]
+      render json: {error: "No se puede crear usuario con id"}, :status => 400
+    else
+      @user = User.new({:name => params[:nombre], :username => params[:usuario] })
+      if @user.save
+        if params[:apellido]
+          @user.lastname = params[:apellido]
+        end
+        if params[:twitter]
+          @user.twitter = params[:twitter]
+        end
+        render json: {id: @user.id, nombre: @user.name, apellido: @user.lastname, usuario: @user.username, twitter: @user.twitter }, :status => 201
+      else
+        render json: {error:"La creación ha fallado"}, :status => 400
+      end
+    end
   end
+
   def show
-    render json: {status:"trying to show one "}
+    if User.exists?(params[:id])
+      @user = User.find(params[:id])
+      render json: {id: @user.id, nombre: @user.name, apellido: @user.lastname, usuario: @user.username, twitter: @user.twitter }, :status => 200
+    else
+      render json: {error:"Usuario no encontrado"}, :status => 404
+    end
   end
+
   def index
     render json: {status:"trying to show all"}
   end
+
   def destroy
     render json: {status:"trying to destroy"}
   end
+
   def edit
     render json: {status:"trying to edit"}
   end
+
+  private
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  validates_presence_of :user, :username
+  #validates_presence_of :user, :username
+  def JSONify
+    render json: {id: @user.id, nombre: @user.name, apellido: @user.lastname, usuario: @user.username, twitter: @user.twitter }
+  end
 end
