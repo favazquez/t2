327532e2c5c5c8b0e7653a8ba348637a8d633771
Felipe <favazquez@uc.cl>
2017-03-31 12:47:32 -0300

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ApplicationRecord
-  #validates_presence_of :user, :username
-  def JSONify
-    render json: {id: @user.id, nombre: @user.name, apellido: @user.lastname, usuario: @user.username, twitter: @user.twitter }
-  end
+  validates_presence_of :name, :username
 end
